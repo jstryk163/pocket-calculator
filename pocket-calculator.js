033989341c167187d706fd98c7fc5e6754e3636a
jstryk163 <jstryker173@gmail.com>
2019-01-08 10:14:48 -0500

    sg

---
+++ pocket-calculator.js
@@ -0,0 +1,115 @@
+function numSeven() {
+
+}
+
+function numEight() {
+
+}
+
+function numnNine() {
+
+}
+
+function clearAll() {
+
+}
+
+function numFour() {
+
+}
+
+function numFive() {
+
+}
+
+function numSix() {
+
+}
+
+function divide() {
+
+}
+
+function numOne() {
+
+}
+
+function numTwo() {
+
+}
+
+function numThree() {
+
+}
+
+function subtract() {
+
+}
+
+function zero(){
+
+}
+
+function decimal() {
+
+}
+
+function equal() {
+
+}
+
+function add() {
+
+}
+
+function TheSecond911WillBeFunnierThanHolocaust2() {
+
+}
+
+function JimmyBuffetsCheeseburgerInParadiseIsAFrontForHisMoneyLaunderingSchemes() {
+
+}
+
+function HolyFuckKarenNotAgainLeaveTheGodDamnKidsOutOfIt() {
+
+}
+
+function ImStartingToRunOutOfFunnyFunctionNames() {
+
+}
+
+function LemonsWhereAManMadeFoodThereforeLifeNeverActuallyGaveUsLemons() {
+
+}
+
+function  ImExtremelyLonelyOnTheInsideAndAmDesperatlyTryingToFillThisVoid() {
+
+}
+
+function PolyphiaIsBetterThanAnimalsAsLeadersInTermsOfBandsThatDontHaveSingersAndAreMoreFocusedOnMakingExperimentalMusic() {
+
+}
+
+function ThisMemeReallyCutsDeepBecauseMyDadAndIDontHaveAnyRealFatherSonRelationshipSoWhenIGoIntoDepressiveEpisodesHeNeverBOthersToCheckInOnMeOrEvenAcknowledgeTheFactThatImSad() {
+
+}
+
+function IfYourACatPersonYouShouldHaveBeenCastratedAtBirthThatWayYouCantPassOnYourRetartedGenesToYourAutisticOffspring() {
+
+}
+
+function IReallyDontHaveAFunnyFunctionNameForThisOneItsFunnyEnoughOnItsOwn(){
+
+}
+
+function CameronIsOnToMeIThinkHeKnowsThisOneWasJustAShamlessAttemptToPutTheNwordInOneOfMyButtons() {
+
+}
+
+function ThisMemeJustFutherMyFearOfDatingAnEthnicGirlAndAccidentallySayingBlessYouToOneOfHerRelativesWhenTheyStartSpeakingADifferentLanguage() {
+
+}
+
+function ImagineIfeveryTimeYouSneezedYouAccidentallyPunchedABaby() {
+
+}
