a378b8c397a8ce86eca6b218687edd94cc1997e5
jstryk163 <jstryker173@gmail.com>
2019-01-08 10:35:45 -0500

    sg

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,115 +1,95 @@
 function numSeven() {
-
+var div = document.getElementById("output")
+str += "7"
+div.InnerHTML = str;
 }
 
 function numEight() {
-
+var div = document.getElementById("output")
+str += "8"
+div.InnerHTML = str;
 }
 
 function numnNine() {
-
+var div = document.getElementById("output")
+str += "9"
+div.InnerHTML = str;
 }
 
 function clearAll() {
-
+var div = document.getElementById("output")
+str += ""
+div.InnerHTML = str;
 }
 
 function numFour() {
-
+var div = document.getElementById("output")
+str += "4"
+div.InnerHTML = str;
 }
 
 function numFive() {
-
+var div = document.getElementById("output")
+str += "5"
+div.InnerHTML = str;
 }
 
 function numSix() {
-
+var div = document.getElementById("output")
+str += "6"
+div.InnerHTML = str;
 }
 
 function divide() {
-
+var div = document.getElementById("output")
+str += "/"
+div.InnerHTML = str;
 }
 
 function numOne() {
-
+var div = document.getElementById("output")
+str += "1"
+div.InnerHTML = str;
 }
 
 function numTwo() {
-
+var div = document.getElementById("output")
+str += "2"
+div.InnerHTML = str;
 }
 
 function numThree() {
-
+var div = document.getElementById("output")
+str += "3"
+div.InnerHTML = str;
 }
 
 function subtract() {
-
+var div = document.getElementById("output")
+str += "-"
+div.InnerHTML = str;
 }
 
 function zero(){
-
+var div = document.getElementById("output")
+str += "0"
+div.InnerHTML = str;
 }
 
 function decimal() {
-
+var div = document.getElementById("output")
+str += "."
+div.InnerHTML = str;
 }
 
 function equal() {
-
+var div = document.getElementById("output")
+str += "="
+div.InnerHTML = str;
 }
 
 function add() {
-
-}
-
-function TheSecond911WillBeFunnierThanHolocaust2() {
-
-}
-
-function JimmyBuffetsCheeseburgerInParadiseIsAFrontForHisMoneyLaunderingSchemes() {
-
-}
-
-function HolyFuckKarenNotAgainLeaveTheGodDamnKidsOutOfIt() {
-
-}
-
-function ImStartingToRunOutOfFunnyFunctionNames() {
-
-}
-
-function LemonsWhereAManMadeFoodThereforeLifeNeverActuallyGaveUsLemons() {
-
-}
-
-function  ImExtremelyLonelyOnTheInsideAndAmDesperatlyTryingToFillThisVoid() {
-
-}
-
-function PolyphiaIsBetterThanAnimalsAsLeadersInTermsOfBandsThatDontHaveSingersAndAreMoreFocusedOnMakingExperimentalMusic() {
-
-}
-
-function ThisMemeReallyCutsDeepBecauseMyDadAndIDontHaveAnyRealFatherSonRelationshipSoWhenIGoIntoDepressiveEpisodesHeNeverBOthersToCheckInOnMeOrEvenAcknowledgeTheFactThatImSad() {
-
-}
-
-function IfYourACatPersonYouShouldHaveBeenCastratedAtBirthThatWayYouCantPassOnYourRetartedGenesToYourAutisticOffspring() {
-
-}
-
-function IReallyDontHaveAFunnyFunctionNameForThisOneItsFunnyEnoughOnItsOwn(){
-
-}
-
-function CameronIsOnToMeIThinkHeKnowsThisOneWasJustAShamlessAttemptToPutTheNwordInOneOfMyButtons() {
-
-}
-
-function ThisMemeJustFutherMyFearOfDatingAnEthnicGirlAndAccidentallySayingBlessYouToOneOfHerRelativesWhenTheyStartSpeakingADifferentLanguage() {
-
-}
-
-function ImagineIfeveryTimeYouSneezedYouAccidentallyPunchedABaby() {
-
+var div = document.getElementById("output")
+str += "+"
+div.InnerHTML = str;
 }
