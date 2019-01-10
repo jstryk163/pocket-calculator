5a4b252dae70ebdc36024f547b986e26d7b3f5eb
jstryk163 <jstryker173@gmail.com>
2019-01-10 10:36:06 -0500

    sg

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,18 +1,18 @@
 function numSeven() {
 var div = document.getElementById("output")
-str += "7"
+str += 7
 div.InnerHTML = str;
 }
 
 function numEight() {
 var div = document.getElementById("output")
-str += "8"
+str += 8
 div.InnerHTML = str;
 }
 
 function numnNine() {
 var div = document.getElementById("output")
-str += "9"
+str += 9
 div.InnerHTML = str;
 }
 
@@ -24,72 +24,72 @@ div.InnerHTML = str;
 
 function numFour() {
 var div = document.getElementById("output")
-str += "4"
+str += 4
 div.InnerHTML = str;
 }
 
 function numFive() {
 var div = document.getElementById("output")
-str += "5"
+str += 5
 div.InnerHTML = str;
 }
 
 function numSix() {
 var div = document.getElementById("output")
-str += "6"
+str += 6
 div.InnerHTML = str;
 }
 
 function divide() {
 var div = document.getElementById("output")
-str += "/"
+str += /
 div.InnerHTML = str;
 }
 
 function numOne() {
 var div = document.getElementById("output")
-str += "1"
+str += 1
 div.InnerHTML = str;
 }
 
 function numTwo() {
 var div = document.getElementById("output")
-str += "2"
+str += 2
 div.InnerHTML = str;
 }
 
 function numThree() {
 var div = document.getElementById("output")
-str += "3"
+str += 3
 div.InnerHTML = str;
 }
 
 function subtract() {
 var div = document.getElementById("output")
-str += "-"
+str += -
 div.InnerHTML = str;
 }
 
 function zero(){
 var div = document.getElementById("output")
-str += "0"
+str += 0
 div.InnerHTML = str;
 }
 
 function decimal() {
 var div = document.getElementById("output")
-str += "."
+str += .
 div.InnerHTML = str;
 }
 
 function equal() {
 var div = document.getElementById("output")
-str += "="
+str += =
 div.InnerHTML = str;
 }
 
 function add() {
 var div = document.getElementById("output")
-str += "+"
+str += +
 div.InnerHTML = str;
 }
